eb07d3d7547825b8c9da7a8d258d9d3895f6d8be
Hoang Van Luyen <vanhoangluyen@gmail.com>
2017-11-14 07:03:39 +0700

    Initial Commit

---
+++ SplitDelegateDataSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplitDelegateDataSource
+//
+//  Created by HoangLuyen on 11/14/17.
+//  Copyright © 2017 HoangLuyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
